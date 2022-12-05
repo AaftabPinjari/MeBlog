4c79e2234cf31c1c1f9488233165a6452282efa8
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2022-12-05 14:43:43 +0530

    done with integrating hygraph

---
+++ components/BlogCard.js
@@ -0,0 +1,11 @@
+export default function BlogCard({ title }) {
+    return <div className="p-10 min-w-screen">
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+        <h1 className="text-white-100 text-2xl" >{title}</h1>
+    </div>
+}
